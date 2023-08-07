7bea1cf16af26606b579bd1177de2a4392c766e6
GuillermoA231 <andradajoaquin302@gmail.com>
2023-08-07 13:12:50 -0300

    maquetado inicial, division de App a Navbar, instala npm

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
-
+import NavBar from './components/common/NavBar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <NavBar/>
     <App />
   </React.StrictMode>,
 )
