7c44a7af179a675c78e9aaefd1943574ff3df380
GuillermoA231 <andradajoaquin302@gmail.com>
2023-08-15 13:29:23 -0300

    progreso tarea 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,18 @@
 
 import './styles/index.css';
 import './styles/features.css';
+import NavBar from './components/common/NavBar';
+import Banner from './components/common/Banner';
+import Novedades from './components/views/Novedades';
 
 const App = () => {
  
 
   return (
     <>
-      <box>
-        <div className='bg-white p-5 text-center'>
-          <div style={{fontSize: '80px', fontWeight: 'bold'}}>HEY DEVELOPERS ..!</div>
-          <div style={{fontSize: '30px'}}>Conoce los nuevos curso RollingCode!</div>
-          <div style={{ marginTop: '20px'}}>
-            <button className='btn btn-primary'>Comprar Ahora!</button>
-          </div>
-        </div>
-      </box>
-
-      <div className="container px-4 py-5" id="featured-3">
-        <h2 className="pb-2 border-bottom text-white">NOVEDADES</h2>
-        <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-          <div className="feature col text-white">
-            <h3 className="fs-2">Featured title</h3>
-            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-            <a href="#" className="text-warning">
-              Call to action
-            </a>
-          </div>
-          <div className="feature col text-white">
-            <h3 className="fs-2">Featured title</h3>
-            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-            <a href="#" className="text-warning">
-              Call to action
-            </a>
-          </div>
-          <div className="feature col text-white">
-            <h3 className="fs-2">Featured title</h3>
-            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-            <a href="#" className="text-warning">
-              Call to action
-
-            </a>
-          </div>
-        </div>
-      </div>
+      <NavBar />
+      <Banner />
+      <Novedades />
     </>
   );
 }
---
+++ src/components/common/Link.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Link() {
+  return (
+    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <li className="nav-item ">
+        <a className="nav-link active text-white" aria-current="page" href="#">
+          Link-1
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active text-white" aria-current="page" href="#">
+          Link-2
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active text-white" aria-current="page" href="#">
+          Link-3
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Link;
